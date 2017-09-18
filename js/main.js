bb75f15eab737a57fda73804c0994fa87f4dae32
Derr Saff <andrewgladilin@gmail.com>
2017-09-19 01:27:55 +0300

    Add language comparison and language tags

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 $(document).ready(function () {
-    $("label").click(function () {
-        $(this).toggleClass("btn-secondary").toggleClass("btn-success");
-        $("." + $(this).data("language")).toggle();
-    })
+  $("th").not("th.service-cell, th.no-language-selected").toggle();
+  $("td").not("td.language-feature, td.no-language-selected").toggle();
+
+  $("label").click(function () {
+    $(this).toggleClass("btn-secondary").toggleClass("btn-success");
+    $("." + $(this).data("language")).toggle();
+
+    if ($("label.btn-success")) {
+      $(".no-language-selected").hide();
+    }
+  })
 });
