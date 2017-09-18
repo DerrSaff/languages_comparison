7d80205952ab466ea88511583e23d5eb9190cf54
Derr Saff <andrewgladilin@gmail.com>
2017-09-18 14:46:46 +0300

    Add toggle function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-$(document).ready( function () {
-    $("label").click( function() {
+$(document).ready(function () {
+    $("label").click(function () {
         $(this).toggleClass("btn-secondary").toggleClass("btn-success");
+        $("." + $(this).data("language")).toggle();
     })
 });
